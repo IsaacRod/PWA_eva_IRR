a1feb3e8c0389de484a68998df47f5c80436ab29
IsaacRod <isaacrr507@gmail.com>
2021-04-13 23:27:02 -0500

    PWA_eva_IRR

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDpuifJ7-Bsqtv1lokucFi32JlI9aTm-Lg",
+  authDomain: "pwaisaac.firebaseapp.com",
+  projectId: "pwaisaac",
+  storageBucket: "pwaisaac.appspot.com",
+  messagingSenderId: "928006539197",
+  appId: "1:928006539197:web:c544e6c488b90f289c74d4"
+});
